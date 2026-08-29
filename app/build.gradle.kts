44a7d5f40027b62a90c7d8d159465d599395a427
Kirill <desvingns@gmail.com>
2026-08-29 22:56:27 +0200

    fix: restore release versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.mysd.android"
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 17
-        versionName = "0.1.16"
+        versionCode = 16
+        versionName = "0.1.15"
     }
 
     buildFeatures {
